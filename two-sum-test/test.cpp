6f2935fcc145325f603bbace4b8601cb31d55e09
Alpha Zeta <b819ujZ132@gmail.com>
2019-04-05 14:50:51 -0400

    Add tests for two-sum.

---
+++ two-sum-test/test.cpp
@@ -0,0 +1,36 @@
+#include "stdafx.h"
+
+#include <algorithm>
+
+#include "CppUnitTest.h"
+#include "../two-sum/Solution.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Test
+{
+  TEST_CLASS(Test)
+  {
+  public:
+    TEST_METHOD(RandomOrder)
+    {
+      const std::vector<int> nums = { 20, 3, 9, -2 };
+      const auto target = 7;
+
+      const auto actual = Solution::twoSum(nums, target);
+
+      const std::vector<unsigned int> expected = { 2, 3 };
+
+      Assert::AreEqual(actual.size(), (size_t)2); // size_t = unsigned int
+      Assert::IsTrue(std::equal(actual.begin(), actual.end(), expected.begin()));
+    }
+
+    TEST_METHOD(NoMatch)
+    {
+      const std::vector<int> nums = { 1, 2, 3 };
+      const auto target = 10;
+
+      Assert::ExpectException<std::exception>([&nums, target] { Solution::twoSum(nums, target); });
+    }
+  };
+}
